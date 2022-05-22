5353f0833197996c12262d7f99b07a34ace0b5f1
Miski Ali <miskiali222@gmail.com>
2022-05-22 16:50:26 -0500

    I finished all the questions except for the stretch goals

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -12,14 +12,26 @@ for (let i=0; i<4; i++) {
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
+for (let i=0; i<6; i++) {  
+  console.log(i);
+}
 console.log('count from 0 to 5');
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
+
+for (let i=3; i<6; i++) {  
+  console.log(i);
+}
+
 console.log('count from 3 to 5');
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
+for (let i=2; i<10; i+=2) {  
+  console.log(i);
+}
+
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
@@ -31,17 +43,46 @@ console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
+
+for (let i=0; i<stars.length; i++) {  
+  console.log('for loop',stars [i]);
+}
+
+
 console.log('Some stars:');
 
 
 // 3. While loops  
+
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
+while (stars){
+  console.log('while in loop. stars:', stars);
+  stars++;
+}
+
+
 console.log('Some stars using while:');
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
+
+let index = 0;
+let max = 10; 
+
+while(index < 6){
+  console.log('While in loop. count:', index);
+  index++;
+}
 console.log('count from 0 to 5');
 
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
+let backwards= 10;
+
+while(backwards>= 5){
+  console.log('count backwards:', backwards);
+   backwards--;
+}
+
 console.log('count backwards from 10 to 5');
